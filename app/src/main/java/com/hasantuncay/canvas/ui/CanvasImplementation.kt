5cf554c70e06570d5143fdd13a548ed415e24080
Hasan Tunçay <hasantuncay2635@gmail.com>
2024-01-13 23:52:08 +0300

    Canvas Class done

--- app/src/main/java/com/hasantuncay/canvas/ui/CanvasImplementation.kt
+++ app/src/main/java/com/hasantuncay/canvas/ui/CanvasImplementation.kt
@@ -1,5 +1,6 @@
 package com.hasantuncay.canvas.ui
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,17 +10,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultBlendMode
+import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,40 +37,40 @@ fun CanvasModifier() {
     val configuration = LocalConfiguration.current
 
 
-    //Screeb width and height in dp
+    //Screen width and height in dp
     val screenWidthDp = configuration.screenWidthDp.dp
     val screenHeightDp = configuration.screenHeightDp.dp
 
 
-    //Screeb width and height in pixel
+    //Screen width and height in pixel
     val screenWidthPx = with(density) { screenWidthDp.toPx() }
     val screenHeightPx = with(density) { screenHeightDp.toPx() }
-val boxHeight=screenHeightDp/4
+    val boxHeight = screenHeightDp / 4
 
     val radius = minOf(
         screenWidthPx,
         screenHeightPx
-    ) /5
+    ) / 5
     val center = Offset(x = screenWidthPx / 2, y = screenHeightPx / 2)
-val colors=listOf(
-    Color.Green,
-    Color.White,
-    Color.Red,
-    Color.White,
-    Color.Blue,
-    Color.White,
-    Color.Yellow
-)
+    val colors = listOf(
+        Color.Green,
+        Color.White,
+        Color.Red,
+        Color.White,
+        Color.Blue,
+        Color.White,
+        Color.Yellow
+    )
 
     val solidColorBrush = SolidColor(Color.Red)
 
     val linearGradientBrush = Brush.linearGradient(
-        colors = listOf(Color.Red, Color.Blue,Color.Red, Color.Blue),
+        colors = listOf(Color.Red, Color.Blue, Color.Red, Color.Blue),
 
 
-    )
+        )
     val radialGradientBrush = Brush.radialGradient(
-        center =  Offset.Unspecified,
+        center = Offset.Unspecified,
         radius = radius,
         colors = colors
     )
@@ -75,7 +80,9 @@ val colors=listOf(
             .fillMaxSize()
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
         Box(modifier = Modifier
             .background(Color.LightGray)
             .fillMaxWidth()
@@ -168,8 +175,128 @@ val colors=listOf(
 
 }
 
+@Composable
+fun CanvasClass() {
+    val density = LocalDensity.current
+    val configuration = LocalConfiguration.current
+
+
+    //Screen width and height in dp
+    val screenWidthDp = configuration.screenWidthDp.dp
+    val screenHeightDp = configuration.screenHeightDp.dp
+
+
+    //Screen width and height in pixel
+    val screenWidthPx = with(density) { screenWidthDp.toPx() }
+    val screenHeightPx = with(density) { screenHeightDp.toPx() }
+    val boxHeight = screenHeightDp / 4
+
+    val radius = minOf(
+        screenWidthPx,
+        screenHeightPx
+    )
+    val center = Offset(x = screenWidthPx / 2, y = screenHeightPx / 2)
+    val colors = listOf(
+        Color.Green,
+        Color.White,
+        Color.Red,
+        Color.White,
+        Color.Blue,
+        Color.White,
+        Color.Yellow
+    )
+
+    val solidColorBrush = SolidColor(Color.Red)
+
+    val linearGradientBrush = Brush.linearGradient(
+        colors = listOf(Color.Red, Color.Blue, Color.Red, Color.Blue),
+
+
+        )
+    val radialGradientBrush = Brush.radialGradient(
+        center = Offset.Unspecified,
+        radius = radius,
+        colors = colors
+    )
+
+    Canvas(modifier = Modifier
+        .fillMaxSize()
+         , onDraw =
+
+    {
+
+                drawCircle(
+                    brush = radialGradientBrush,
+                    radius = radius/5,
+                     center = Offset(x = screenWidthPx / 2, y = screenHeightPx / 2),
+                    alpha = 0.6f,
+                    style = Fill,
+                    colorFilter = null,
+                    blendMode = DefaultBlendMode
+                )
+
+
+
+                drawCircle(
+                    brush = linearGradientBrush,
+                    radius = radius/5,
+                    center = Offset(x = screenWidthPx / 3, y = screenHeightPx / 3),
+                    alpha = 0.6f,
+                    style = Fill,
+                    colorFilter = null,
+                    blendMode = DefaultBlendMode
+                )
+
+
+
+
+                drawRect(
+                    brush = linearGradientBrush,
+                    topLeft = Offset(x=screenWidthPx/4,y=screenHeightPx-200),
+
+size= Size(width = 100.dp.toPx(), height = 50.dp.toPx()),
+                    alpha = 0.6f,
+                    style =   Fill,
+                    colorFilter = null,
+                    blendMode = DefaultBlendMode
+                )
+
+
+
+
+
+                drawCircle(
+                    color = Color.Black,
+                    radius = radius/5,
+                    center =Offset(x = screenWidthPx / 4, y = screenHeightPx / 4),       //this.center,
+                    alpha = 0.6f,
+                    style = Fill,
+                    colorFilter = null,
+                    blendMode = DefaultBlendMode
+                )
+
+
+
+
+                drawCircle(
+                    brush = solidColorBrush,
+                    radius = radius/5,
+                    center = Offset(x = screenWidthPx / 5, y = screenHeightPx / 5),       //this.center,
+                    alpha = 0.6f,
+                    style = Fill,
+                    colorFilter = null,
+                    blendMode = DefaultBlendMode
+                )
+
+
+
+    }
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewCanvas() {
-    CanvasModifier()
+    CanvasClass()
+    //CanvasModifier()
 }
